7dcaae5313faf71e6115f67d231d2f11be6dc82e
Jose R. Pacheco <josep@mit.edu>
2013-01-26 15:54:27 -0500

    full annots

--- pbots_calc-master/python/calculator.sh
+++
@@ -1,3 +0,0 @@
-#!/bin/sh
-export LD_LIBRARY_PATH=/home/ubuntu/pokerbots2013/pbots_calc-master/export/linux2/lib:$LD_LIBRARY_PATH
-python /home/ubuntu/pokerbots2013/pbots_calc-master/python/calculator.py $@
